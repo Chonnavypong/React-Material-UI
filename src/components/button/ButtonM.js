8ddd085161ca79b07a41f1db957098ce5bdfafab
Chonnavypong <chonnavypong@gmail.com>
2020-03-28 15:25:49 +0700

    Material exercise 1

---
+++ src/components/button/ButtonM.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import './Button.scss'
+import Button from '@material-ui/core/Button'
+
+export const ButtonM = () => {
+    return (
+        <div>
+            <h2> Button Component d </h2>
+            <Button variant="contained" color="primary">
+                Hello Button
+            </Button>
+        </div>
+    )
+}
